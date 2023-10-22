2abdc0f2abf505d569ed57e54d853db7144edbc8
Rokas <rookas.rudzenskas@gmail.com>
2023-10-22 09:48:53 +0300

    fix

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import {StatusBar} from "expo-status-bar";
 
 export default function TabOneScreen() {
   return (
