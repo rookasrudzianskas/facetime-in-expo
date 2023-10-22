cd6f6febc2441451c95a8809ae69a595e1d2b125
Rokas <rookas.rudzenskas@gmail.com>
2023-10-22 11:28:25 +0300

    fix

--- src/app/(app)/index.tsx
+++ src/app/(app)/index.tsx
@@ -1,13 +1,20 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>
-        byrookas 🚀
-      </Text>
+    <View className="flex-1">
+      <Stack.Screen options={{ title: 'FaceTime' }} />
+
+      <View className="flex flex-row justify-between items-center">
+        <TouchableOpacity activeOpacity={0.7} className="bg-blue-500 px-4 py-2">
+          <Text>Create FaceTime</Text>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.7} className="bg-blue-500 px-4 py-2">
+          <Text>Join FaceTime</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
