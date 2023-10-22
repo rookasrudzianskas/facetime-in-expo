ce28b49ea148a582a4a9500be778f3a0a83fd758
Rokas <rookas.rudzenskas@gmail.com>
2023-10-22 11:42:36 +0300

    more data

--- src/app/(app)/call.tsx
+++ src/app/(app)/call.tsx
@@ -14,8 +14,8 @@ const callId = 'default_cfac32cf-afb2-49ab-ad78-655c2604da5d';
 
 export default function ModalScreen() {
   const client = useStreamVideoClient();
-  const [call] = useState(() => client?.call('default', callId));
-  const res = useCalls();
+  const calls = useCalls();
+  const call = calls[0];
 
   // useEffect(() => {
   //   call?.join({ create: true });
