15b4637e48e32ecdaaa59d35c9aa0dc1d5f945c2
Rokas <rookas.rudzenskas@gmail.com>
2023-10-22 10:36:09 +0300

    fix

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,7 @@ import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import { PermissionsAndroid, Platform } from 'react-native';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -34,6 +35,18 @@ export default function RootLayout() {
       SplashScreen.hideAsync();
     }
   }, [loaded]);
+
+  useEffect(() => {
+    const run = async () => {
+      if (Platform.OS === 'android') {
+        await PermissionsAndroid.requestMultiple([
+          'android.permission.POST_NOTIFICATIONS',
+          'android.permission.BLUETOOTH_CONNECT',
+        ]);
+      }
+    };
+    run();
+  }, []);
 
   if (!loaded) {
     return null;
