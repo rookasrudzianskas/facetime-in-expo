71e90f4b7afc672712e8c10ce6a8d7f4e15a3e29
Rokas <rookas.rudzenskas@gmail.com>
2023-10-22 11:21:45 +0300

    fix

--- src/lib/stream.ts
+++ src/lib/stream.ts
@@ -1,7 +1,7 @@
 import {StreamVideoClient, User} from "@stream-io/video-react-native-sdk";
 import {useState} from "react";
 
-const apiKey = 'r56ue2bjerbr';
+const apiKey = process.env.EXPO_PUBLIC_STREAM_API_KEY!;
 const userId = 'rokas';
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicm9rYXMifQ.wC88UAO34NQFOXCxSMS7fuqdip_38kDoM9a18Wx9q7k';
 const callId = 'default_cfac32cf-afb2-49ab-ad78-655c2604da5d';
