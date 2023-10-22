593918aab70d9a8ba2e622080135a83be87a8c99
Rokas <rookas.rudzenskas@gmail.com>
2023-10-22 10:49:53 +0300

    fix

--- src/app/modal.tsx
+++ src/app/modal.tsx
@@ -12,13 +12,25 @@ import { useEffect, useState } from 'react';
 const apiKey = 'r56ue2bjerbr';
 const userId = 'rokas';
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicm9rYXMifQ.wC88UAO34NQFOXCxSMS7fuqdip_38kDoM9a18Wx9q7k';
-const callId = 'my-call-id';
+const callId = 'test123';
 const user: User = { id: userId };
 
 export default function ModalScreen() {
+  const [client] = useState(
+    () => new StreamVideoClient({ apiKey, user, token }),
+  );
+
+  const [call] = useState(() => client.call('default', callId));
+
+  useEffect(() => {
+    call.join({ create: true });
+  }, [call]);
+
   return (
     <View style={styles.container}>
-
+      <StreamVideo client={client}>
+        <StreamCall call={call}>{/* Your UI */}</StreamCall>
+      </StreamVideo>
     </View>
   );
 }
