ec83df748cfa6861e4170eeef984db2db156a59e
Rokas <rookas.rudzenskas@gmail.com>
2023-10-22 11:50:05 +0300

    global styles

--- src/app/(app)/join.tsx
+++ src/app/(app)/join.tsx
@@ -1,13 +1,20 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TextInput, Button} from 'react-native';
 
 const JoinCall = () => {
+
+  const onJoin = () => {
+
+  }
+
   return (
-    <View>
-      <Text>
-        byrookas 🚀
+    <View className="flex flex-col items-center justify-center">
+      <Text className="text-lg">
+        Join a call
       </Text>
+      <TextInput placeholder={'enter id of the facetime'} className="h-10 w-full border" />
+      <Button title={'Join'} onPress={onJoin} />
     </View>
   );
 };
