afff9a640333f0e988aabd4cdb89fe261b1e5a20
Rokas <rookas.rudzenskas@gmail.com>
2023-10-22 12:37:45 +0300

    fix

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,23 +1,13 @@
-import {Stack} from "expo-router";
+import {Redirect, Stack} from "expo-router";
 import {useAuth} from "../../context/AuthProvider";
 import {ActivityIndicator, View} from "react-native";
-import Account from "./account";
-import Auth from "./auth";
 
 const AppLayout = () => {
   const {session, loading} = useAuth();
 
-  if(loading) {
-    return (
-      <View className='h-screen flex items-center justify-center'>
-        <ActivityIndicator />
-      </View>
-    )
-  }
-
-  return <>
-    {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
-  </>
+  if(loading) { return (<View className='h-screen flex items-center justify-center'><ActivityIndicator /></View>)}
+  if(!session) return <Redirect href={'/auth'} />
+  return <Stack />
 }
 
 export default AppLayout;
