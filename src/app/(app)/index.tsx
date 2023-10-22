067d28d236379450a4f24feaf11e0fd0cb1291dc
Rokas <rookas.rudzenskas@gmail.com>
2023-10-22 11:32:25 +0300

    fix

--- src/app/(app)/index.tsx
+++ src/app/(app)/index.tsx
@@ -1,14 +1,26 @@
 //@ts-nocheck
 import React from 'react';
 import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import {useStreamVideoClient} from "@stream-io/video-react-native-sdk";
 
 const HomeScreen = () => {
+  const client = useStreamVideoClient();
+
+  const onCreateCall = () => {
+    if(!client) {
+      return;
+    }
+
+    const callId = 'default_testing123';
+    client.call('default', callId);
+  }
+
   return (
     <View className="flex-1">
       <Stack.Screen options={{ title: 'FaceTime' }} />
 
       <View className="flex flex-row justify-between items-center">
-        <TouchableOpacity activeOpacity={0.7} className="bg-blue-500 px-4 py-2">
+        <TouchableOpacity onPress={onCreateCall} activeOpacity={0.7} className="bg-blue-500 px-4 py-2">
           <Text>Create FaceTime</Text>
         </TouchableOpacity>
         <TouchableOpacity activeOpacity={0.7} className="bg-blue-500 px-4 py-2">
