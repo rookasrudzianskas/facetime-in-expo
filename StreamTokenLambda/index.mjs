bd30aaa86821c832717e03e195ed7213e2c4d025
Rokas <rookas.rudzenskas@gmail.com>
2023-10-27 18:59:10 +0300

    .env

--- StreamTokenLambda/index.mjs
+++ StreamTokenLambda/index.mjs
@@ -1,15 +1,11 @@
 import {StreamChat} from "stream-chat";
 import {createClient} from "@supabase/supabase-js";
 
-// const SUPABASE_URL = process.env.SUPABASE_URL ?? '';
-// const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY ?? '';
-const SUPABASE_URL = 'https://izhyhtedfukdnoruqdza.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6aHlodGVkZnVrZG5vcnVxZHphIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc5NjQ5NDgsImV4cCI6MjAxMzU0MDk0OH0.uQ23QDfJHV0K5f5s0wVaf2o7K_EioAPCHb5JNB0RUF8';
+const SUPABASE_URL = process.env.SUPABASE_URL ?? '';
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY ?? '';
 
-// const STREAM_API_KEY = process.env.STREAM_API_KEY ?? '';
-// const STREAM_API_SECRET = process.env.STREAM_API_SECRET ?? '';
-const STREAM_API_KEY = 'r56ue2bjerbr';
-const STREAM_API_SECRET = 'armbc8q8gyegkbnyfea9kx486cr56cz6had9e7bacm2t5sakcdg3xjvjfccvn4sd';
+const STREAM_API_KEY = process.env.STREAM_API_KEY ?? '';
+const STREAM_API_SECRET = process.env.STREAM_API_SECRET ?? '';
 
 const user_id = 'john'
 
