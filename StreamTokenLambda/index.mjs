56cd16b805030376f1ff7e615d40a2256ef1ab82
Rokas <rookas.rudzenskas@gmail.com>
2023-10-27 18:39:07 +0300

    fix

--- StreamTokenLambda/index.mjs
+++ StreamTokenLambda/index.mjs
@@ -1,3 +1,13 @@
+// Define values.
+const api_key = 'r56ue2bjerbr'
+const api_secret = 'armbc8q8gyegkbnyfea9kx486cr56cz6had9e7bacm2t5sakcdg3xjvjfccvn4sd'
+const user_id = 'john'
+
+// Initialize a Server Client
+const serverClient = StreamChat.getInstance( api_key, api_secret);
+// Create User Token
+const token = serverClient.createToken(user_id);
+
 export const handler = async (event) => {
   // TODO implement
   const response = {
