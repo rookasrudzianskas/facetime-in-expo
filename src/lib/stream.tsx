1264bedf8ebb5f4f9820ee04296c9333fb26fd5e
Rokas <rookas.rudzenskas@gmail.com>
2023-10-28 09:47:59 +0300

    fix

--- src/lib/stream.tsx
+++ src/lib/stream.tsx
@@ -1,7 +1,9 @@
 import {StreamVideo, StreamVideoClient, User } from '@stream-io/video-react-native-sdk';
-import { PropsWithChildren } from 'react';
+import {PropsWithChildren, useEffect} from 'react';
+import {useAuth} from "../context/AuthProvider";
 
 const apiKey = process.env.EXPO_PUBLIC_STREAM_API_KEY || '';
+const LAMBDA_URL = 'https://jccwdukwfiohnkotclpdzjskm40kmpbz.lambda-url.us-east-1.on.aws'
 const userId = 'rokas';
 const token =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidmFkaW0ifQ.NV_QYM9kLATmjB21QX3_ZkhHj21d8sKOil3703tBrZQ';
@@ -10,5 +12,17 @@ const user: User = { id: userId };
 export const client = new StreamVideoClient({ apiKey  });
 
 export const StreamClientProvider = ({ children }: PropsWithChildren) => {
+  const {session} = useAuth();
+
+  useEffect(() => {
+    if(!session?.access_token) return;
+    const fetchToken = async () => {
+      const result = await fetch(
+        `${LAMBDA_URL}/?token=${session?.access_token}`
+      )
+    }
+    fetchToken();
+  }, [session?.access_token]);
+
   return <StreamVideo client={client}>{children}</StreamVideo>;
 }
